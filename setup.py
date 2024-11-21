e5b109a3db24dcc14228b5dde62b621f76144a8c
Karol <karoliss@hotmail.com>
2024-11-21 18:58:49 +0200

    Update 2

--- setup.py
+++ setup.py
@@ -2,5 +2,9 @@ from setuptools import setup, find_packages
 
 setup(
     name ="karolis_s_mod1_atsiskaitymas",
-    packages = find_packages()
+    version ="1.0.0",
+    packages = find_packages(),
+    install_requires=[
+        #pip install requests, lxml.html, csv
+    ]
 )
---
+++ tests/__init__.py
@@ -0,0 +1,1 @@
+from main import gesintuvu_kainos
